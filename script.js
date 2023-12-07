6f5cc12ced0df510dae5aa9db503f3ad3f3284da
0latoye <140350157+0latoye@users.noreply.github.com>
2023-12-07 09:49:43 +0100

    updates

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // API to pull currency Data https://exchangeratesapi.io/
+// Api-key:9acaa3b48899a68b380cd5ebedc7da24
+
+class CurrencyExchangeAPI {
+    constructor(apiKey) {
+      this.apiKey 
+  = apiKey;
+      this.baseURL = "https://api.exchangeratesapi.io";
+    }
+  
+    async getExchangeRate(baseCurrency, targetCurrency) {
+      const response = await fetch(
+        `${this.baseURL}/latest?access_key=${this.apiKey}&base=${baseCurrency}`
+      );
+      const data = await response.json();
+      const exchangeRate = data.rates[targetCurrency];
+      if (exchangeRate) {
+        return exchangeRate;
+      } else {
+        throw new Error(`Could not get exchange rate for ${targetCurrency}`);
+      }
+    }
+  
+    async convertCurrency(baseCurrency, targetCurrency, amount) {
+      const exchangeRate = await this.getExchangeRate(baseCurrency, targetCurrency);
+      return amount * exchangeRate;
+    }
+  }
+  
+  // Example usage:
+  const apiKey = "9acaa3b48899a68b380cd5ebedc7da24";
+  const api = new CurrencyExchangeAPI(apiKey);
+  api.convertCurrency("USD", "EUR", 1)
+    .then(result => console.log(result))
+    .catch(error => console.error(error));
