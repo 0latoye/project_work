1d0533e3e0abd579b4e7c88628033eb74c9a53ed
0latoye <140350157+0latoye@users.noreply.github.com>
2023-12-08 12:47:12 +0100

    updates

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
-// API to pull currency Data https://exchangeratesapi.io/
-// Api-key:9acaa3b48899a68b380cd5ebedc7da24
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
 
-class CurrencyExchangeAPI {
-    constructor(apiKey) {
-      this.apiKey 
-  = apiKey;
-      this.baseURL = "https://api.exchangeratesapi.io";
+for (let i = 0; i < dropList.length; i++) {
+    for(let currency_code in country_list){
+        // selecting USD by default as FROM currency and NPR as TO currency
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "NPR" ? "selected" : "";
+        // creating option tag with passing currency code as a text and value
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        // inserting options tag inside select tag
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
-  
-    async getExchangeRate(baseCurrency, targetCurrency) {
-      const response = await fetch(
-        `${this.baseURL}/latest?access_key=${this.apiKey}&base=${baseCurrency}`
-      );
-      const data = await response.json();
-      const exchangeRate = data.rates[targetCurrency];
-      if (exchangeRate) {
-        return exchangeRate;
-      } else {
-        throw new Error(`Could not get exchange rate for ${targetCurrency}`);
-      }
+    dropList[i].addEventListener("change", e =>{
+        loadFlag(e.target); // calling loadFlag with passing target element as an argument
+    });
+}
+
+function loadFlag(element){
+    for(let code in country_list){
+        if(code == element.value){ // if currency code of country list is equal to option value
+            let imgTag = element.parentElement.querySelector("img"); // selecting img tag of particular drop list
+            // passing country code of a selected currency code in a img url
+            imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
+        }
     }
-  
-    async convertCurrency(baseCurrency, targetCurrency, amount) {
-      const exchangeRate = await this.getExchangeRate(baseCurrency, targetCurrency);
-      return amount * exchangeRate;
+}
+
+window.addEventListener("load", ()=>{
+    getExchangeRate();
+});
+
+getButton.addEventListener("click", e =>{
+    e.preventDefault(); //preventing form from submitting
+    getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector("form .icon");
+exchangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value; // temporary currency code of FROM drop list
+    fromCurrency.value = toCurrency.value; // passing TO currency code to FROM currency code
+    toCurrency.value = tempCode; // passing temporary currency code to TO currency code
+    loadFlag(fromCurrency); // calling loadFlag with passing select element (fromCurrency) of FROM
+    loadFlag(toCurrency); // calling loadFlag with passing select element (toCurrency) of TO
+    getExchangeRate(); // calling getExchangeRate
+})
+
+function getExchangeRate(){
+    const amount = document.querySelector("form input");
+    const exchangeRateTxt = document.querySelector("form .exchange-rate");
+    let amountVal = amount.value;
+
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
     }
-  }
-  
-  // Example usage:
-  const apiKey = "9acaa3b48899a68b380cd5ebedc7da24";
-  const api = new CurrencyExchangeAPI(apiKey);
-  api.convertCurrency("USD", "EUR", 1)
-    .then(result => console.log(result))
-    .catch(error => console.error(error));
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/1fcd6134fba4158ccf87fb97/latest/${fromCurrency.value}`;
+    // fetching api response and returning it with parsing into js obj and in another then method receiving that obj
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
+        let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    }).catch(() =>{ // if user is offline or any other error occured while fetching data then catch function will run
+        exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
